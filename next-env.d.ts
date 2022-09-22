41cb00488035026f9d70a3f85d8ca337065748bd
Junaid Malik <junmalik109@gmail.com>
2022-09-22 13:23:24 +0100

    a

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+NEXT_PUBLIC_API_URL="79f0edad-3b1b-4e38-ac3d-e0b67b5992ea"
+API_KEY="f4c94a7c-bebf-4676-9e1d-970d2e3cbee8"
